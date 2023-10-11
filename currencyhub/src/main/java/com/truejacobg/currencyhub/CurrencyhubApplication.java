0ab0eda199d8f6aa9ecbce03007b2c81a2c9e81e
TrueJacobG <jakubgradzewicz1309@gmail.com>
2023-10-11 18:39:30 +0200

    backend - currency hub

---
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/CurrencyhubApplication.java
@@ -0,0 +1,13 @@
+package com.truejacobg.currencyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyhubApplication.class, args);
+	}
+
+}
---
+++ currencyhub/src/test/java/com/truejacobg/currencyhub/CurrencyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truejacobg.currencyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
