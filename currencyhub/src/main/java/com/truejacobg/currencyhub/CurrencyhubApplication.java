3d5dcbbb7f99c6de5095d6a266c362f71b343e70
SARUXSARU <lukasz.muras25@gmail.com>
2023-10-15 21:48:56 +0200

    working connection to test db

--- currencyhub/src/main/java/com/truejacobg/currencyhub/CurrencyhubApplication.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/CurrencyhubApplication.java
@@ -1,13 +1,26 @@
 package com.truejacobg.currencyhub;
 
+import com.mongodb.client.MongoCollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 @SpringBootApplication
 public class CurrencyhubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyhubApplication.class, args);
+
+		MongoClient mongoClient = MongoClients.create("mongodb+srv://client:client@sarektest.dbiwcvg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp");
+		MongoDatabase db = mongoClient.getDatabase("currencyHub");
+		MongoCollection col = db.getCollection("User");
+		Document doc = new Document("_id","2").append("name","Amelia Smith");
+		col.insertOne(doc);
+
 	}
 
 }
