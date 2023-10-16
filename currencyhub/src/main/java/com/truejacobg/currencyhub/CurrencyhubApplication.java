ca3c47005f5f119d212c0d7f6d22414207ad9006
TrueJacobG <jakubgradzewicz1309@gmail.com>
2023-10-16 20:02:00 +0200

    gitignore add .yml file

--- currencyhub/src/main/java/com/truejacobg/currencyhub/CurrencyhubApplication.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/CurrencyhubApplication.java
@@ -1,22 +1,16 @@
 package com.truejacobg.currencyhub;
 
-import com.mongodb.client.MongoCollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-
 @SpringBootApplication
 public class CurrencyhubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyhubApplication.class, args);
 
-		MongoClient mongoClient = MongoClients.create("mongodb+srv://client:client@sarektest.dbiwcvg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp");
-		//MongoDatabase db = mongoClient.getDatabase("currencyHub");
+
+		//MongoClient mongoClient = MongoClients.create("mongodb+srv://client:client@" + "will Be Imported!");
+		//MongoDatabase db = mongoClient.getDatabase("will Be Imported!");
 		//MongoCollection col = db.getCollection("User");
 		//Document doc = new Document("_id","2").append("name","Amelia Smith");
 		//col.insertOne(doc);
