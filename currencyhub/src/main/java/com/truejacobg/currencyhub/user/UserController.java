e60ad241d21014ed794f2418bf42bdb170f91f56
nergamed <114864594+GoldFireGames@users.noreply.github.com>
2023-11-06 18:58:50 +0100

    Added GetMapping, PutMapping, DeleteMapping

--- currencyhub/src/main/java/com/truejacobg/currencyhub/user/UserController.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/user/UserController.java
@@ -3,11 +3,10 @@ package com.truejacobg.currencyhub.user;
 import com.truejacobg.currencyhub.user.dto.CreateUserResponseDTO;
 import com.truejacobg.currencyhub.user.dto.UserDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // trzyma konkretny endpoint, zarzadzaja co sie kiedy ma zadziac, ktore metody
 // jaki typ zwracany itp. te klasy słóżą jaki link ma się zając czym. Kolejność metod staramy sie trzymać CRUDowo
@@ -25,4 +24,21 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/{email}")
+    ResponseEntity<CreateUserResponseDTO> getUser(@PathVariable String email){
+        CreateUserResponseDTO response = userService.getUser(email);
+        return  ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{email}")
+    ResponseEntity<CreateUserResponseDTO> upadateUser(@RequestBody UserDTO userDTO, @PathVariable String email){
+        CreateUserResponseDTO response = userService.updateUser(userDTO,email);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{email}")
+    ResponseEntity<CreateUserResponseDTO> deleteUser(@PathVariable String email){
+        CreateUserResponseDTO response = userService.deleteUser(email);
+        return ResponseEntity.ok(response);
+    }
 }
--- currencyhub/src/main/java/com/truejacobg/currencyhub/user/UserService.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/user/UserService.java
@@ -4,8 +4,10 @@ import com.truejacobg.currencyhub.user.dto.CreateUserResponseDTO;
 import com.truejacobg.currencyhub.user.dto.UserDTO;
 import com.truejacobg.currencyhub.user.entity.UserEntity;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 // prawdziwy backend metody walidacje, zabawa z bazą, prawdziwe mięso, tutaj bedzie cały kod
 // musi implementować repo, 90% będzie tutaj, autoryzacje, zabezpieczenia, uruchomienia, rzucanie błędów
 //
@@ -16,11 +18,44 @@ public class UserService {
     private UserRepository userRepository;
 
     public CreateUserResponseDTO createUser(UserDTO userDTO) {
-        UserEntity userEntity = new UserEntity("123124", userDTO.getName(), userDTO.getAuthCode(), userDTO.getSurname(), userDTO.getEmail(), userDTO.getCreationDate());
+        UserEntity userEntity = new UserEntity("100", userDTO.getName(), userDTO.getAuthCode(), userDTO.getSurname(), userDTO.getEmail(), userDTO.getCreationDate());
         userRepository.save(userEntity);
 
         return new CreateUserResponseDTO("ok", HttpStatus.ACCEPTED);
     }
 
 
+    public CreateUserResponseDTO getUser(String userEmail) {
+        UserEntity user =  userRepository.findByEmail(userEmail);
+        if( user != null){
+            // found
+            return new CreateUserResponseDTO("has been found", HttpStatus.OK);
+        }
+        else{
+            return new CreateUserResponseDTO("has not been found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public CreateUserResponseDTO updateUser(UserDTO userDTO, String email) {
+        // is he in database?
+        UserEntity user = userRepository.findByEmail(email);
+        if(user == null){
+            return new CreateUserResponseDTO("User has not been in db", HttpStatus.NOT_FOUND);
+        }
+        else {
+            user.setName(userDTO.getName());
+            user.setSurname(userDTO.getSurname());
+            user.setEmail(userDTO.getEmail());
+
+            userRepository.save(user);
+
+            return new CreateUserResponseDTO("The user has been updated successfully.", HttpStatus.OK);
+
+        }
+    }
+
+    public CreateUserResponseDTO deleteUser(String email) {
+        userRepository.deleteByEmail(email); // TODO: dodać wyjątek
+        return new CreateUserResponseDTO("User has been deleted",HttpStatus.OK);
+    }
 }
