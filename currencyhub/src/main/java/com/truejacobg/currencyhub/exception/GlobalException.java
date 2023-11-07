324c859597724d46b5c4991a528e07e2aa3d913d
SARUXSARU <lukasz.muras25@gmail.com>
2023-11-07 23:25:15 +0100

    @SARUXSARU - add global exception handler - exception parent class

--- currencyhub/src/main/java/com/truejacobg/currencyhub/exception/GlobalException.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/exception/GlobalException.java
@@ -1,9 +1,7 @@
 package com.truejacobg.currencyhub.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+
 public class GlobalException extends RuntimeException{
     public GlobalException(String message){
         super(message);
