361f198060b3f6fd042662ed2b5f256cbc656b2b
KubaPlayer1 <rewizjapl@gmail.com>
2023-11-22 22:12:10 +0100

    @KubaPlayer1 view of currences

--- frontend/src/screen/BaseScreen.tsx
+++ frontend/src/screen/BaseScreen.tsx
@@ -1,14 +1,41 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useAtom } from "jotai";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { loggedUserAtom } from "../jotai/loggedUserAtom";
 import { ScreenNaviagtion } from "../type/ScreenNavigation";
 import { User } from "../type/User";
+import React, { useEffect, useState } from "react";
 
 type Props = NativeStackScreenProps<ScreenNaviagtion, "Home">;
 
 const BaseScreen = ({ navigation }: Props) => {
   const [loggedUser, setLoggedUser] = useAtom<User>(loggedUserAtom);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/v1/currency/rate"
+      );
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const renderListItem = ({ item }) => (
+    <View style={{ padding: 10 }}>
+      <Text>{item.currencyCode}</Text>
+      <Text>{item.currencyName}</Text>
+      <Text>{item.mid}</Text>
+    </View>
+  );
 
   return (
     <View>
@@ -17,7 +44,13 @@ const BaseScreen = ({ navigation }: Props) => {
       <View>
         <Text>Email: {loggedUser.email}</Text>
       </View>
-      
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={data}
+          renderItem={renderListItem}
+          keyExtractor={(item) => item.currencyCode.toString()}
+        />
+      </View>
     </View>
   );
 };
