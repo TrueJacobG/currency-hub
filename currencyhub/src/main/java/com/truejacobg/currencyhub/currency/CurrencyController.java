cb26898fb71ea3e573062cf7502c120f357cdba1
SARUXSARU <lukasz.muras25@gmail.com>
2023-12-04 19:50:06 +0100

    @SARUXSARU - add date validation and currency/rate/week/month/year endpoints

--- currencyhub/src/main/java/com/truejacobg/currencyhub/currency/CurrencyController.java
+++ currencyhub/src/main/java/com/truejacobg/currencyhub/currency/CurrencyController.java
@@ -3,8 +3,11 @@ package com.truejacobg.currencyhub.currency;
 import com.truejacobg.currencyhub.currency.dto.CurrencyDateResponseDTO;
 import com.truejacobg.currencyhub.currency.dto.CurrencyResponseDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @AllArgsConstructor
@@ -15,22 +18,39 @@ public class CurrencyController {
     private CurrencyService currencyService;
 
     @GetMapping("/rate")
-    ResponseEntity<CurrencyResponseDTO> getCurrencyRates(){
+    ResponseEntity<CurrencyResponseDTO> getCurrencyRates() {
         CurrencyResponseDTO responseDTO = currencyService.getCurrencyRates();
         return ResponseEntity.ok(responseDTO);
     }
 
     @GetMapping("/rate/{currencyCode}")
-    ResponseEntity<CurrencyResponseDTO> getCurrencyRateByCode(@PathVariable String currencyCode){
+    ResponseEntity<CurrencyResponseDTO> getCurrencyRateByCode(@PathVariable String currencyCode) {
         CurrencyResponseDTO responseDTO = currencyService.getCurrencyRateByCode(currencyCode);
         return ResponseEntity.ok(responseDTO);
     }
 
     @GetMapping("/rate/{currencyCode}/{from}/{end}")
-    ResponseEntity<CurrencyDateResponseDTO> getCurrencyRateByCodeInDate(@PathVariable String currencyCode, @PathVariable String from, @PathVariable String end){
+    ResponseEntity<CurrencyDateResponseDTO> getCurrencyRateByCodeInDate(@PathVariable String currencyCode, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
         CurrencyDateResponseDTO responseDTO = currencyService.getCurrencyRateByCodeInDate(currencyCode, from, end);
         return ResponseEntity.ok(responseDTO);
     }
 
+    @GetMapping("/rate/{currencyCode}/week")
+    ResponseEntity<CurrencyDateResponseDTO> getCurrencyRateCurrentWeek(@PathVariable String currencyCode) {
+        CurrencyDateResponseDTO responseDTO = currencyService.getCurrencyRateCurrentWeek(currencyCode);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/rate/{currencyCode}/month")
+    ResponseEntity<CurrencyDateResponseDTO> getCurrencyRateCurrentMonth(@PathVariable String currencyCode) {
+        CurrencyDateResponseDTO responseDTO = currencyService.getCurrencyRateCurrentMonth(currencyCode);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/rate/{currencyCode}/year")
+    ResponseEntity<CurrencyDateResponseDTO> getCurrencyRateCurrentYear(@PathVariable String currencyCode) {
+        CurrencyDateResponseDTO responseDTO = currencyService.getCurrencyRateCurrentYear(currencyCode);
+        return ResponseEntity.ok(responseDTO);
+    }
 
 }
